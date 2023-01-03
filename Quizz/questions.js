cb4c29b86d06aac787ac9d57ef46c64d76c4c7f4
Tryns <113093354+Tryns@users.noreply.github.com>
2023-01-03 08:59:09 +0100

    Quizz

---
+++ Quizz/questions.js
@@ -0,0 +1,83 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Quel est la capital de la France ?",
+    answer: "Paris",
+    options: [
+      "Paris",
+      "Bordeau",
+      "Lille",
+      "Draguignan"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Où ce situe la Normandie ?",
+    answer: "Nord-Ouest",
+    options: [
+      "Nord",
+      "Nord-Ouest",
+      "Ouest",
+      "Centre"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Quelle a été la première vidéo Youtube ?",
+    answer: "Vidéo dans un zoo",
+    options: [
+      "Vidéo de Michael Jackson",
+      "Vidéo dans un zoo",
+      "Vidéo de jacquie-Michel version premium",
+      "Vidéo de malikay se battant contre une mouette"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Quel est le meilleur joueur de League of Legends ?",
+    answer: "malikay",
+    options: [
+      "malikay",
+      "Thomas",
+      "Nabil",
+      "Dédé"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Quel est le fidèle pokémon de Sasha ?",
+    answer: "Pikachu",
+    options: [
+      "Poussifeu",
+      "Yuumi",
+      "Pikachu",
+      "Alakazam"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Quel est la pire Viper sur valorant ?",
+    answer: "Nabil",
+    options: [
+      "Malikay",
+      "Thomas",
+      "Matis",
+      "Nabil"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 7,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
